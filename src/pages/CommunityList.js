92fdab38d5ebd93a4af2356f53eea06365633717
Shaurya Vardhan Singh <gsshaurya@gmail.com>
2022-09-19 19:35:52 +0530

    community-list initial UI

--- src/pages/CommunityList.js
+++ src/pages/CommunityList.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import "./communityList.scss";
+import addWhite from "../assets/Icons/plus_colored.svg";
 
 const CommunityList = () => {
   const address = useSelector((x) => x.auth.accountAddress);
+  const [searchText, setSearchText] = useState("");
 
   const renderHeader = () => (
     <div
@@ -28,16 +31,38 @@ const CommunityList = () => {
     </div>
   );
 
+  const communityLists = [1, 2, 3, 4, 5, 6];
+
   return (
-    <div
-      style={{
-        width: "100vw",
-        display: "flex",
-        justifyContent: "space-between",
-        flexDirection: "column",
-      }}
-    >
+    <div className="community-list-container">
       {renderHeader()}
+      <div className="community-list-title">List of Graphs</div>
+      <div className="community-list-search">
+        <input
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          placeholder="search graphs"
+        />
+      </div>
+      <div className="community-list-wrapper">
+        {communityLists.map((ele, index) => (
+          <div key={index} className="community-list-card">
+            <div className="community-list-card-image"></div>
+            <div className="community-list-card-content">
+              <div className="community-list-card-title">The Catalyzer</div>
+              <div className="community-list-card-description">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </div>
+              <div className="community-list-card-button">
+                <img src={addWhite} alt="" />
+                Follow
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
